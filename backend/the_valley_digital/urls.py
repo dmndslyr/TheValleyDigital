aef9f3cbaad0009376ab420bf7c3b7419b9f1be9
dmndslyr <edrienecabanela@gmail.com>
2024-11-14 15:44:00 +0800

    Changed url path for superadmin

--- backend/the_valley_digital/urls.py
+++ backend/the_valley_digital/urls.py
@@ -20,10 +20,10 @@ from articles import views  # Import views from the current app
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('admin1/login/', views.admin_login, name='admin-login'),
+    path('superadmin/', admin.site.urls),
+    path('admin/login/', views.admin_login, name='admin-login'),
     path('admin/articles/', views.article_list, name='admin_article_list'),
-    path('admin1/article/<int:id>/', views.article_detail, name='article_detail'),
+    path('admin/article/<int:id>/', views.article_detail, name='article_detail'),
     path('admin/search/', views.article_search, name='admin_article_search'),  
     path('admin/create/', views.article_create, name='admin_article_create'),
     path('admin/article/<int:id>/modify/', views.article_update, name='admin_article_modify'),
