9ef2f9abc62738e8906f286edf3b9624d532cd29
dmndslyr <edrienecabanela@gmail.com>
2024-12-07 20:33:45 +0800

    Added tags in the Model

--- backend/articles/admin.py
+++ backend/articles/admin.py
@@ -1,6 +1,13 @@
 # Register your models here.
 from django.contrib import admin
-from .models import Category, Articles
+from .models import Category, Articles, Tag
 
 admin.site.register(Category)
-admin.site.register(Articles)
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Articles)
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display = ('headline', 'category', 'author', 'is_published', 'publication_date')
+    filter_horizontal = ('tags',)  # Makes selecting tags easier in the admin interface
