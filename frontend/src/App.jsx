dc92a87b79f65a0421cb77286307d08b1614a888
Ely-Bea <frieschicken92@gmail.com>
2025-01-01 21:01:59 +0800

    added about us, searchpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import Header from './components/Header';  // Adjust path if necessary
 import ArticlePage from './pages/ArticlePage';
 import AboutUs from './pages/AboutUs';
+import SearchInterface from './pages/SearchInterface';
+import LoginPage from './pages/Login';
 import PrintIssue from './pages/PrintIssue';
 import Footer from './components/Footer';  // Adjust path if necessary
+import './App.css'
 
 const App = () => {
   return (
     <>
     <Header></Header>
-    <PrintIssue></PrintIssue>
+    <SearchInterface></SearchInterface>
     <Footer></Footer>
     </>
   );
--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -19,7 +19,108 @@ function AboutUs(){
             </div>
         </div>
         <div className='about-us-content'>
-            <p></p>
+            <p><b>EDITORIAL BOARD | SY 2024-2025</b></p>
+            <table>
+                <tr>
+                    <td><b>EDITOR-IN-CHIEF</b></td>
+                    <td>JUAN DELA CRUZ</td>
+                </tr>
+                <tr>
+                    <td><b>ASSOCIATE EDITOR</b></td>
+                    <td>JUAN DELA CRUZ</td>
+                </tr>
+                <tr>
+                    <td><b>MANAGING EDITOR</b></td>
+                    <td>JUAN DELA CRUZ</td>
+                </tr>
+                <tr>
+                    <td><b>NEWS EDITOR</b></td>
+                    <td>JUAN DELA CRUZ</td>
+                </tr>
+                <tr>
+                    <td><b>FEATURE EDITOR</b></td>
+                    <td>JUAN DELA CRUZ</td>
+                </tr>
+                <tr>
+                    <td><b>SCI-TECH EDITOR</b></td>
+                    <td>JUAN DELA CRUZ</td>
+                </tr>
+                <tr>
+                    <td><b>SPORTS EDITOR</b></td>
+                    <td>JUAN DELA CRUZ</td>
+                </tr>
+                <tr>
+                    <td><b>EDITORIAL CARTOONIST</b></td>
+                    <td>JUAN DELA CRUZ</td>
+                </tr>
+            </table>
+            <p><b>CONTRIBUTORS</b></p>
+            <table>
+                <tr>
+                    <td>JUAN DELA CRUZ</td>
+                    <td>JUAN DELA CRUZ</td>
+                </tr>
+                <tr>
+                    <td>JUAN DELA CRUZ</td>
+                    <td>JUAN DELA CRUZ</td>
+                </tr>
+                <tr>
+                    <td>JUAN DELA CRUZ</td>
+                    <td>JUAN DELA CRUZ</td>
+                </tr>
+            </table>
+            <div className='faculty'>
+                <h4>EDITHA O. CABANELA</h4>
+                <p>ADVISER - ENGLISH</p>
+            </div>
+            <div className='faculty'>
+                <h4>ALJON B. FERNANDEZ</h4>
+                <p>CO-ADVISER - SHS</p>
+            </div>
+            <div className='faculty'>
+                <h4>EDDUARDO C. OCAMPO</h4>
+                <p>PRINCIPAL III</p>
+            </div>
+        </div>
+        <div className='about-us-development'>
+            <div className='about-us-dleft'>
+                <img src={logo} alt='logo'></img>
+                <p>website</p>
+            </div>
+            <div className='about-us-middle'></div>
+            <div className='about-us-middle'></div>
+            <div className='about-us-dright'></div>
+        </div>
+        <div className='about-us-content'>
+            <div className='department-info'>
+                <p><b>DEVELOPMENT TEAM</b></p>
+                <p><b>CPE 3A | PANGASINAN STATE UNIVERSITY - URDANETA CAMPUS</b></p>
+            </div>
+            <table className='developers'>
+                <tr>
+                    <td>ALMUETE, JAMELLA IRA NACES</td>
+                    <td>ANTIPOLO, PEARL ORMITA</td>
+                </tr>
+                <tr>
+                    <td>CABANELLA, EDRIENE JAY ORIBELLO</td>
+                    <td>DUEÑAS, VICTOR EDWARD RAFANAN</td>
+                </tr>
+                <tr>
+                    <td>ESPARAGUERA, ELYZA BEA DOMINGO</td>
+                    <td>GALLARDE, KRISHA CORNELIO</td>
+                </tr>
+            </table>
+            <p className='requirement-txt'>FINAL REQUIREMENT FOR THE COURSE 'CPE 125- WEB DEVELOPMENT'</p>
+            <h4><b>ENGR. JEDDIE M. ZARATE</b></h4>
+            <div className='department-info'>            
+                <p>INSTRUCTOR</p>
+                <p>COMPUTER ENGINEERING DEPARTMENT</p>
+                <p>PANGASINAN STTATE UNIVERSITY URDANETA CAMPUS</p>
+            </div>
+            <div className='copyright'>
+                <p>TO GOD BE THE GLORY</p>
+                <p>COPYRIGHT 2024</p>
+            </div>
         </div>
         </>
     );
