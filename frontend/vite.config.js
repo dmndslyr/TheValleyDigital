fe1ebec055e05f9af873982dac28de9f427c40fa
dmndslyr <edrienecabanela@gmail.com>
2025-01-24 23:39:43 +0800

    vite js config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-  build: {
-    target: 'esnext', // Ensure modern JavaScript compatibility
-    outDir: 'dist', // Make sure the output folder is 'dist'
-  },
 })
