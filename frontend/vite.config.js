0261d46d3277c39397f294084eef75d822809f68
dmndslyr <edrienecabanela@gmail.com>
2025-01-24 23:37:23 +0800

    for deployment trial

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  build: {
+    target: 'esnext', // Ensure modern JavaScript compatibility
+    outDir: 'dist', // Make sure the output folder is 'dist'
+  },
 })
