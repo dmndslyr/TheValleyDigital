ace2e975a129e4218584ccfc5b952ee3a152b293
Ubuntu <ubuntu@ip-172-31-7-146.ap-southeast-2.compute.internal>
2025-01-29 14:12:51 +0000

    Changes from EC2

--- backend/the_valley_digital/settings.py
+++ backend/the_valley_digital/settings.py
@@ -33,14 +33,15 @@ CORS_ALLOW_ALL_ORIGINS = ["http://localhost:5173/", "https://thevalleydigital.ne
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Ensure this path is correct
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Whitenoise to handle static files efficiently
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
